617bb3ef038253b7bc9d2d056f70773333651746
Van Viet Hieu Anh <vanviethieuanh@gmail.com>
2024-09-22 17:52:37 +0700

    All scripts

--- hog_race.sh
+++ hog_race.sh
@@ -1,5 +1,5 @@
-# Extract Owner/Repo from CSV
-tail -n +2 search_results.csv | awk -F, '{print $2}' >repos.txt
+# Extract Owner/Repo from CSV and get distinct lines without sorting
+tail -n +2 "$1" | awk -F, '{print $2}' | uniq > repos.txt
 
 # Use GNU Parallel to scan repositories
 parallel -a repos.txt ./scan_repo.sh
--- scan_repo.sh
+++ scan_repo.sh
@@ -4,6 +4,6 @@
 OWNER_REPO=$1
 
 # Run TruffleHog
-trufflehog git --no-verification --no-update --json "https://github.com/${OWNER_REPO}" > "results/${OWNER_REPO//\//_}_results.json"
+trufflehog git --no-update --json "https://github.com/${OWNER_REPO}" > "results/${OWNER_REPO//\//_}_results.json"
 
 echo "Scanned ${OWNER_REPO}, results saved to ${OWNER_REPO//\//_}_results.json"
